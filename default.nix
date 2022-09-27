3040896049505613d599b7fa88401be4907147ff
Lawrence Dunn <dunnla@seas.upenn.edu>
2022-09-27 19:41:09 -0400

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  lib = pkgs.lib;
+  mylib = import <nixpkgs/pkgs/build-support/coq/extra-lib.nix> {
+    inherit lib;
+  };
+in with mylib; pkgs.coqPackages.mkCoqDerivation {
+    pname = "tealeaves";
+    owner = "dunnl";
+    version = ./.;
+    propagatedBuildInputs =
+      [ ];
+    meta = {
+      description = "A library for generic reasoning about syntax.";
+      license = lib.licenses.mit;
+    };
+  }
