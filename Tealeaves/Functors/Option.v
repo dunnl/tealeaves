612a700e811d893aa89c1b2ff2a8b4313839b571
Lawrence Dunn <dunnla@seas.upenn.edu>
2022-10-31 21:20:51 -0400

    Rename Maybe -> Option

--- Tealeaves/Functors/Option.v
+++ Tealeaves/Functors/Option.v
@@ -1,52 +1,54 @@
 From Tealeaves Require Export
   Classes.Algebraic.Monad.
 
-(** * Maybe monad *)
+(** * [option] monad *)
 (******************************************************************************)
+(*
 Inductive Maybe (A : Type) :=
 | Just : A -> Maybe A
 | None : Maybe A.
 
 Arguments Just {A}%type_scope _.
 Arguments None {A}%type_scope.
+*)
 
-#[export] Instance Fmap_Maybe : Fmap Maybe :=
-  fun A B (f : A -> B) (m : Maybe A) =>
+#[export] Instance Fmap_option : Fmap option :=
+  fun A B (f : A -> B) (m : option A) =>
     match m with
-    | Just a => Just (f a)
+    | Some a => Some (f a)
     | None => None
     end.
 
-#[export] Instance Functor_Maybe : Functor Maybe.
+#[export] Instance Functor_option : Functor option.
 Proof.
   constructor.
   - intros. now ext [|].
   - intros. now ext [|].
 Qed.
 
-#[export] Instance Return_Maybe : Return Maybe :=
-  fun A => Just.
+#[export] Instance Return_option : Return option :=
+  fun A => Some.
 
-#[export] Instance Join_Maybe : Join Maybe :=
-  fun A (m : Maybe (Maybe A)) =>
+#[export] Instance Join_option : Join option :=
+  fun A (m : option (option A)) =>
     match m with
-    | Just m' => m'
+    | Some m' => m'
     | None => None
     end.
 
-#[export] Instance: Natural (@ret Maybe _).
+#[export] Instance: Natural (@ret option _).
 Proof.
   constructor; try typeclasses eauto.
   reflexivity.
 Qed.
 
-#[export] Instance: Natural (@join Maybe _).
+#[export] Instance: Natural (@join option _).
 Proof.
   constructor; try typeclasses eauto.
   intros. now ext [[|]|].
 Qed.
 
-#[export] Instance Monad_Maybe : Monad Maybe.
+#[export] Instance Monad_option : Monad option.
 Proof.
   constructor; try typeclasses eauto.
   - intros. now ext [|].
