58b5e184e3c0e530d4aeecb4d6b77316faeea266
Lawrence Dunn <dunnla@seas.upenn.edu>
2023-02-27 15:02:14 -0500

    Clean Data/Propositions.v

--- Tealeaves/Data/Prop.v
+++ Tealeaves/Data/Prop.v
@@ -2,29 +2,21 @@ From Tealeaves Require Import
   Classes.Monoid.
 
 (** * Propositional monoids *)
-Section propositional_monoids.
+#[export] Instance Monoid_op_or: Monoid_op Prop := or.
 
-  #[global] Instance: Monoid_op Prop := and.
+#[export] Instance Monoid_unit_false: Monoid_unit Prop := False.
 
-  #[global] Instance: Monoid_op Prop := or.
+#[program, export] Instance Monoid_disjunction: @Monoid Prop or False.
 
-  #[global] Instance: Monoid_unit Prop := False.
+#[export] Instance Monoid_op_and: Monoid_op Prop := and.
 
-  #[global] Instance: Monoid_unit Prop := True.
+#[export] Instance Monoid_unit_true: Monoid_unit Prop := True.
 
-  #[global] Program Instance Monoid_disjunction : @Monoid Prop or False.
+#[program, export] Instance Monoid_conjunction: @Monoid Prop and True.
 
-  #[global] Program Instance Monoid_conjunction : @Monoid Prop and True.
+Solve All Obligations with intros; propext; firstorder.
 
-  Solve All Obligations with intros; propext; firstorder.
+#[program, export] Instance Monmor_neg_disj_conj :
+  @Monoid_Morphism Prop Prop or False and True not.
 
-  (* This won't hold in a non-classical setting *)
-  (* #[global] Program Instance Monmor_neg_conj_disj : @Monoid_Morphism Prop Prop and True or False not.
-   *)
-
-  #[global] Program Instance Monmor_neg_disj_conj :
-    @Monoid_Morphism Prop Prop or False and True not.
-
-  Solve Obligations with intros; propext; firstorder.
-
-End propositional_monoids.
+Solve Obligations with intros; propext; firstorder.
