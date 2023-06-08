d6ca52550831e3449475ecc3598dce54ea992f8a
Lawrence Dunn <dunnla@seas.upenn.edu>
2023-06-08 10:07:34 -0400

    Experiment making Monad.v and Dec/Functor.v more terse

--- Tealeaves/Classes/Decorated/Functor.v
+++ Tealeaves/Classes/Decorated/Functor.v
@@ -4,57 +4,37 @@ From Tealeaves Require Export
 
 Import Product.Notations.
 
-(** * Operation <<fmapdt>> *)
+(** * Decorated functor *)
 (******************************************************************************)
-Section operations.
 
-  Context
-    (E : Type)
-    (F : Type -> Type).
-
-  Class Fmapd :=
-    fmapd : forall {A B : Type} (f : E * A -> B), F A -> F B.
-
-End operations.
+(** ** Operation <<fmapd>> *)
+(******************************************************************************)
+Class Fmapd (E : Type) (F : Type -> Type) :=
+  fmapd : forall {A B : Type} (f : E * A -> B), F A -> F B.
 
 Arguments fmapd {E}%type_scope F%function_scope {Fmapd} {A B}%type_scope f%function_scope _.
 
 (** ** Typeclass *)
 (******************************************************************************)
-Section class.
-
-  Context
-    (E : Type)
-    (T : Type -> Type)
-    `{Fmapd E T}.
-
-  Class DecoratedFunctor :=
-    { dfun_fmapd1 : forall (A : Type),
-        fmapd T (extract (E ×)) = @id (T A);
-      dfun_fmapd2 : forall (A B C : Type) (g : E * B -> C) (f : E * A -> B),
-        fmapd T g ∘ fmapd T f = fmapd T (g ∘ cobind (E ×) f);
-    }.
-
-End class.
+Class DecoratedFunctor (E : Type) (T : Type -> Type) `{Fmapd E T} :=
+  { dfun_fmapd1 : forall (A : Type),
+      fmapd T (extract (E ×)) = @id (T A);
+    dfun_fmapd2 : forall (A B C : Type) (g : E * B -> C) (f : E * A -> B),
+      fmapd T g ∘ fmapd T f = fmapd T (g ∘ cobind (E ×) f);
+  }.
 
 #[local] Generalizable Variables E T.
 
-(** * To functor *)
+(** ** Derived functor instance *)
 (******************************************************************************)
-Module ToFunctor.
+Module DerivedInstances.
 
-  Section operation.
+  #[export] Instance Fmap_Fmapd
+    (T : Type -> Type)
+    `{Fmapd E T} : Fmap T :=
+  fun (A B : Type) (f : A -> B) => fmapd T (f ∘ extract (E ×)).
 
-    Context
-      (T : Type -> Type)
-      `{Fmapd E T}.
-
-  #[export] Instance Fmap_Fmapd : Fmap T :=
-      fun (A B : Type) (f : A -> B) => fmapd T (f ∘ extract (E ×)).
-
-  End operation.
-
-  Section with_kleisli.
+  Section with_instance.
 
     Context
       (T : Type -> Type)
@@ -64,14 +44,14 @@ Module ToFunctor.
         fmap T (@id A) = @id (T A).
     Proof.
       intros. unfold_ops @Fmap_Fmapd.
-      now rewrite (dfun_fmapd1 E T).
+      now rewrite (dfun_fmapd1 (T := T) (E := E)).
     Qed.
 
     Lemma fmap_fmap : forall (A B C : Type) (f : A -> B) (g : B -> C),
         fmap T g ∘ fmap T f = fmap T (g ∘ f).
     Proof.
       intros. unfold_ops @Fmap_Fmapd.
-      rewrite (dfun_fmapd2 E T).
+      rewrite dfun_fmapd2.
       fequal. reassociate ->.
       now rewrite (kcom_cobind0 (E ×)).
     Qed.
@@ -81,32 +61,28 @@ Module ToFunctor.
         fun_fmap_fmap := fmap_fmap;
       |}.
 
-    Lemma fmap_fmapd : forall (A B C : Type)
-                         (g : B -> C)
-                         (f : E * A -> B),
-        fmap T g ∘ fmapd T f =
-          fmapd T (g ∘ f).
+    Lemma fmap_fmapd :
+      forall (A B C : Type)
+        (g : B -> C)
+        (f : E * A -> B),
+        fmap T g ∘ fmapd T f = fmapd T (g ∘ f).
     Proof.
       intros. unfold_ops @Fmap_Fmapd.
-      rewrite (dfun_fmapd2 E T).
+      rewrite dfun_fmapd2.
       reassociate ->.
       now rewrite (kcom_cobind0 (E ×)).
     Qed.
 
-    (*
     Lemma fmapd_fmap: forall (A B C : Type)
                         (g : E * B -> C)
                         (f : A -> B),
         fmapd T g ∘ fmap T f = fmapd T (g ∘ fmap (E ×) f).
     Proof.
       intros. unfold_ops @Fmap_Fmapd.
-      rewrite (dfun_fmapd2 E T).
-      unfold_ops @Fmap_Cobind.
-      rewrite <- (fmap_to_cobind (E ×)).
-      reflexivity.
+      rewrite dfun_fmapd2.
+      now rewrite <- (fmap_to_cobind (E := E)).
     Qed.
-    *)
 
-  End with_kleisli.
+  End with_instance.
 
-End ToFunctor.
+End DerivedInstances.
