ea1ac93eca2bf8e72a391411363ba5e6e740c96d
Lawrence Dunn <lawrence@comono.id>
2023-06-18 19:26:25 -0400

    Add LN/Atom.v

--- Tealeaves/Backends/LN/Atom.v
+++ Tealeaves/Backends/LN/Atom.v
@@ -3,7 +3,7 @@ From Tealeaves Require Export
   Functors.List
   Classes.EqDec_eq.
 
-Import Classes.Setlike.Functor.Notations.
+Import Functors.Sets.ElNotations.
 
 (** * An opaque type of atoms *)
 (******************************************************************************)
