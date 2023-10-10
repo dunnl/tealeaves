876461fa310f4e4a37981e0222b68f62830ff0e1
Lawrence Dunn <lawrence@comono.id>
2023-10-10 15:20:56 -0400

    Reset everything

--- Tealeaves/Backends/LN.v
+++
@@ -1,7 +0,0 @@
-From Tealeaves.Backends.LN Require Export Atom AtomSet AssocList LN.
-
-Module Notations.
-  Export AtomSet.Notations.
-  Export AssocList.Notations.
-  Export LN.Notations.
-End Notations.
--- Tealeaves/Functors/Identity.v
+++
@@ -1,9 +0,0 @@
-From Tealeaves Require Export
-  Classes.Functor.
-
-(** * The identity functor *)
-(******************************************************************************)
-#[export] Instance Map_I : Map (fun A => A) :=
-  fun (A B : Type) (f : A -> B) => f.
-
-#[program, export] Instance Functor_I : Functor (fun A => A).
