ad5ae94a7f6fd91bccc72ab791c3c9f7f51856e2
Lawrence Dunn <lawrence@comono.id>
2023-10-12 14:38:36 -0400

    Add several common functors

---
+++ Tealeaves/Functors/Diagonal.v
@@ -0,0 +1,25 @@
+From Tealeaves Require Export
+  Classes.Categorical.Applicative.
+
+(** * Diagonal (duplication) applicative functor *)
+(******************************************************************************)
+Definition dup (A : Type) : Type := A * A.
+
+#[export] Instance Map_dup : Map dup :=
+  fun A B f => map_both f : A * A -> B * B.
+
+#[export, program] Instance End_dup : Functor dup.
+
+Solve All Obligations with
+    (intros; now ext [?]).
+
+#[export] Instance Pure_dup : Pure dup :=
+  fun A a => (a, a).
+
+#[export] Instance Mult_dup : Mult dup :=
+  fun A B (x : dup A * dup B) => ((fst (fst x), fst (snd x)), (snd (fst x), snd (snd x))).
+
+#[export, program] Instance Applicative_dup : Applicative dup.
+
+Solve Obligations with
+    (unfold dup; intros; now destruct_all_pairs).
