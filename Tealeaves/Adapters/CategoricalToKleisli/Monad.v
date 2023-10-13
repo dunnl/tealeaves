50114c086f4a786b004cbffd32b46d65a2027484
Lawrence Dunn <lawrence@comono.id>
2023-10-13 14:50:37 -0400

    Add categorical ~> Kleisli Monad.v

---
+++ Tealeaves/Adapters/CategoricalToKleisli/Monad.v
@@ -0,0 +1,141 @@
+From Tealeaves Require Import
+  Classes.Categorical.Monad.
+
+#[local] Generalizable Variables T A B C.
+
+(** * Algebraic monad to Kleisli monad *)
+(******************************************************************************)
+Module ToKleisli.
+
+  Export Kleisli.Monad.
+  Import Kleisli.Monad.Notations.
+
+  Section operation.
+
+    Context
+      (T : Type -> Type)
+      `{Map T} `{Join T}.
+
+    #[export] Instance Bind_Join : Bind T T :=
+      fun {A B} (f : A -> T B) => join T ∘ map T f.
+
+  End operation.
+
+  Section with_monad.
+
+    Context
+      (T : Type -> Type)
+      `{Categorical.Monad.Monad T}.
+
+    (** *** Identity law *)
+    (******************************************************************************)
+    Lemma mon_bind_id :
+      `(bind T (ret T A) = @id (T A)).
+    Proof.
+      intros. unfold_ops @Bind_Join.
+      now rewrite (mon_join_map_ret (T := T)).
+    Qed.
+
+    (** *** Composition law *)
+    (******************************************************************************)
+    Lemma mon_bind_bind : forall (A B C : Type) (g : B -> T C) (f : A -> T B),
+        bind T g ∘ bind T f = bind T (g ⋆1 f).
+    Proof.
+      introv. unfold transparent tcs.
+      unfold kc1.
+      unfold_ops @Bind_Join.
+      rewrite <- 2(fun_map_map (F := T)).
+      reassociate <- on right.
+      reassociate <- on right.
+      unfold_compose_in_compose.
+      rewrite <- (mon_join_join (T := T)).
+      change (map ?T (map ?T g)) with (map (T ∘ T) g).
+      reassociate -> near (map (T ∘ T) g).
+      rewrite <- (natural (ϕ := @join T _)).
+      reflexivity.
+    Qed.
+
+    (** *** Unit law *)
+    (******************************************************************************)
+    Lemma mon_bind_comp_ret : forall (A B : Type) (f : A -> T B),
+        bind T f ∘ ret T A = f.
+    Proof.
+      intros. unfold transparent tcs.
+      reassociate -> on left.
+      unfold_compose_in_compose; (* these rewrites are not visible *)
+        change (@compose A) with (@compose ((fun A => A) A)).
+      rewrite natural.
+      reassociate <- on left.
+      now rewrite (mon_join_ret (T := T)).
+    Qed.
+
+    #[export] Instance Kleisli_Monad : Kleisli.Monad.Monad T :=
+      {| kmon_bind0 := @mon_bind_comp_ret;
+        kmon_bind1 := @mon_bind_id;
+        kmon_bind2 := @mon_bind_bind;
+      |}.
+
+  End with_monad.
+
+End ToKleisli.
+
+(** ** Derived laws *)
+(******************************************************************************)
+Section ToKleisli_laws.
+
+  Context
+    (T : Type -> Type)
+   `{Monad T}.
+
+  Generalizable All Variables.
+  Import Kleisli.Monad.Notations.
+  Import ToKleisli.
+
+  Lemma map_to_bind : forall `(f : A -> B),
+      map T f = bind T (ret T B ∘ f).
+  Proof.
+    intros. unfold_ops @Bind_Join.
+    rewrite <- (fun_map_map (F := T)).
+    reassociate <-.
+    rewrite (mon_join_map_ret (T := T)).
+    reflexivity.
+  Qed.
+
+  Corollary bind_map : forall `(g : B -> T C) `(f : A -> B),
+      bind T g ∘ map T f = bind T (g ∘ f).
+  Proof.
+    intros. unfold transparent tcs.
+    now rewrite <- (fun_map_map (F := T)).
+  Qed.
+
+  Corollary map_bind : forall `(g : B -> C) `(f : A -> T B),
+      map T g ∘ bind T f = bind T (map T g ∘ f).
+  Proof.
+    intros. unfold transparent tcs.
+    reassociate <- on left.
+    rewrite (natural). unfold_ops @Map_compose.
+    now rewrite <- (fun_map_map (F := T)).
+  Qed.
+
+  (** ** Left identity law *)
+  Theorem kleisli_id_l : forall `(f : A -> T B),
+      (@ret T _ B) ⋆1 f = f.
+  Proof.
+    intros. unfold kc1. now rewrite (kmon_bind1 (T := T)).
+  Qed.
+
+  (** ** Right identity law *)
+  Theorem kleisli_id_r : forall `(g : B -> T C),
+      g ⋆1 (@ret T _ B) = g.
+  Proof.
+    intros. unfold kc1. now rewrite (kmon_bind0 (T := T)).
+  Qed.
+
+  (** ** Associativity law *)
+  Theorem kleisli_assoc : forall `(h : C -> T D) `(g : B -> T C) `(f : A -> T B),
+      h ⋆1 (g ⋆1 f) = (h ⋆1 g) ⋆1 f.
+  Proof.
+    intros. unfold kc1 at 3. now rewrite <- (kmon_bind2 (T := T)).
+  Qed.
+
+End ToKleisli_laws.
