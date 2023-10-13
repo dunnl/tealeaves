b180882e0b2b980acf0141979dc017021b54aaf5
Lawrence Dunn <lawrence@comono.id>
2023-10-13 14:35:55 -0400

    Add spec for decorated monads in category DecoratedFunctor.v

--- Tealeaves/Categories/DecoratedFunctor.v
+++ Tealeaves/Categories/DecoratedFunctor.v
@@ -559,3 +559,79 @@ Section DecoratedFunctor_zero_composition.
   Qed.
 
 End DecoratedFunctor_zero_composition.
+
+(** * Decorated monads in terms of monad homomorphisms *)
+(******************************************************************************)
+Section DecoratedMonad_characterization.
+
+  Context
+    `{Categorical.Monad.Monad T} `{Decorate W T} `{Monoid W}
+    `{! Categorical.DecoratedFunctor.DecoratedFunctor W T}.
+
+  (** <<ret T>> commutes with decoration if and only if <<dec T>> maps
+     <<ret T>> to <<ret (T ∘ W)>> *)
+  Lemma dec_ret_iff {A} :
+    (dec T ∘ ret T A = ret T (W * A) ∘ dec (fun x => x) (A:=A)) <->
+    (dec T ∘ ret T A = ret (T ∘ prod W) A).
+  Proof with auto.
+    split...
+  Qed.
+
+  (** <<join T>> commutes with decoration if and only if <<dec T>>
+     maps <<join T>> to <<join (T ∘ prod W)>> of <<dec T ∘ map T (dec T)>>
+     (in other words iff <<dec T>> commutes with <<join>>). *)
+  Lemma dec_join_iff {A} :
+    `(dec T ∘ join T = join T ∘ dec (T ∘ T) (A := A)) <->
+    `(dec T ∘ join T = join (T ∘ prod W) ∘ dec T ∘ map T (dec T (A:=A))).
+  Proof.
+    enough (Heq : join T ∘ dec (A := A) (T ∘ T)
+                  = join (T ∘ prod W) ∘ dec T ∘ map T (dec T))
+      by (split; intro hyp; now rewrite hyp).
+    unfold_ops @Join_Beck @Decorate_compose @BeckDistribution_strength.
+    repeat reassociate <-. fequal. fequal.
+    rewrite (natural (ϕ := @join T _)).
+    unfold_ops @Map_compose. reassociate -> on right.
+    unfold_compose_in_compose.
+    rewrite (fun_map_map (F := T)).
+    unfold shift.
+    rewrite incr_spec.
+    reflexivity.
+  Qed.
+
+  (* TODO Prove (T ∘ prod W) is a monad *)
+  (*
+  Theorem decorated_monad_compatibility_spec :
+    Monad_Hom T (T ∘ prod W) (@dec W T _) <->
+    DecoratePreservingTransformation (fun A => A) T (ret T) /\
+    DecoratePreservingTransformation (T ∘ T) T (@join T _).
+  Proof with auto.
+    split.
+    - introv mhom. inverts mhom. inverts mhom_domain. split.
+      + constructor...
+        introv. symmetry. rewrite dec_ret_iff. apply mhom_ret.
+      + constructor...
+        introv. symmetry. rewrite dec_join_iff. apply mhom_join.
+    - intros [h1 h2]. inverts h1. inverts h2.
+      constructor; try typeclasses eauto.
+      + admit.
+      + admit.
+      + introv  rewrite <- (dec_ret_iff)...
+      + introv. rewrite <- dec_join_iff...
+  Qed.
+
+  Theorem decorated_monad_spec :
+    Categorical.DecoratedMonad.DecoratedMonad W T <->
+      Monad_Hom T (T ∘ prod W) (@dec W T _).
+  Proof with try typeclasses eauto.
+    rewrite decorated_monad_compatibility_spec.
+    split; intro hyp.
+    - inversion hyp. constructor...
+      + constructor... intros. now rewrite (dmon_ret (W := W) (T := T)).
+      + constructor... intros. now rewrite (dmon_join (W := W) (T := T)).
+    - destruct hyp as [hyp1 hyp2]. constructor...
+      + intros. inversion hyp1. now rewrite dectrans_commute.
+      + intros. inversion hyp2. now rewrite <- dectrans_commute.
+  Qed.
+   *)
+
+End DecoratedMonad_characterization.
