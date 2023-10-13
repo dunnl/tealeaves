bc95a51d8668550c81a8f493f9655c660480bd09
Lawrence Dunn <lawrence@comono.id>
2023-10-13 16:09:00 -0400

    Add Kleisli ~> categorical Monad.v

---
+++ Tealeaves/Adapters/KleisliToCategorical/Monad.v
@@ -0,0 +1,58 @@
+From Tealeaves Require Export
+  Classes.Categorical.Monad
+  Classes.Kleisli.Monad.
+
+(** * Categorical monads to Kleisli monads *)
+(******************************************************************************)
+#[export] Instance Join_Bind
+  (T : Type -> Type)
+  `{Return T} `{Bind T T} : Join T :=
+  fun A => bind T (@id (T A)).
+
+(* Import functor instance *)
+Import Kleisli.Monad.DerivedInstances.
+
+Section proofs.
+
+  Context
+    (T : Type -> Type)
+    `{Classes.Kleisli.Monad.Monad T}.
+
+  Instance: Classes.Categorical.Monad.Monad T.
+  Proof.
+    constructor.
+    - typeclasses eauto.
+    - constructor.
+      typeclasses eauto.
+      typeclasses eauto.
+      intros. unfold transparent tcs.
+      now rewrite (kmon_bind0 (T := T)).
+    - constructor.
+      typeclasses eauto.
+      typeclasses eauto.
+      intros. unfold transparent tcs.
+      unfold_compose_in_compose.
+      do 2 rewrite (kmon_bind2 (T := T)).
+      fequal. unfold kc1.
+      reassociate <- on right.
+      now rewrite (kmon_bind0 (T := T)).
+    - intros. unfold transparent tcs.
+      unfold_compose_in_compose.
+      now rewrite (kmon_bind0 (T := T)).
+    - intros. unfold transparent tcs.
+      unfold_compose_in_compose.
+      rewrite (kmon_bind2 (T := T)).
+      unfold kc1.
+      reassociate <- near (bind T (@id (T A))).
+      rewrite (kmon_bind0 (T := T)). unfold compose.
+      now rewrite <- (kmon_bind1 (T := T) A) at 1.
+    - intros. unfold transparent tcs.
+      unfold_compose_in_compose.
+      rewrite (kmon_bind2 (T := T)).
+      rewrite (kmon_bind2 (T := T)).
+      unfold kc1.
+      reassociate <- near (bind T (@id (T A))).
+      now rewrite (kmon_bind0 (T := T)).
+  Qed.
+
+End proofs.
