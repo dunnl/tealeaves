3c87a3ffb3f6bbea0f65bd510811c66ee3e782c5
Lawrence Dunn <lawrence@comono.id>
2023-10-13 16:01:20 -0400

    Add categorical ~> Kleisi for RightModule.v

--- Tealeaves/Adapters/CategoricalToKleisli/Monad.v
+++ Tealeaves/Adapters/CategoricalToKleisli/Monad.v
@@ -1,5 +1,6 @@
 From Tealeaves Require Import
-  Classes.Categorical.Monad.
+  Classes.Categorical.Monad
+  Classes.Categorical.RightModule.
 
 #[local] Generalizable Variables T A B C.
 
@@ -139,3 +140,74 @@ Section ToKleisli_laws.
   Qed.
 
 End ToKleisli_laws.
+
+
+(** * Right modules *)
+(******************************************************************************)
+Module ToKleisliRightModule.
+
+  Import ToKleisli.
+  Export Kleisli.Monad.
+  Import Kleisli.Monad.Notations.
+
+  #[export] Instance Bind_RightAction (F T : Type -> Type) `{Map F} `{RightAction F T} : Bind T F :=
+    fun {A B} (f : A -> T B) => right_action F ∘ map F f.
+
+  Section Monad_kleisli_laws.
+
+    Context
+      (T F : Type -> Type)
+      `{Categorical.RightModule.RightModule F T}.
+
+    (** *** Identity law *)
+    Lemma bind_id :
+      `(bind F (ret T A) = @id (F A)).
+    Proof.
+      intros. unfold transparent tcs.
+      now rewrite (mod_action_map_ret).
+    Qed.
+
+    (** *** Composition law *)
+    Lemma bind_bind : forall (A B C : Type) (g : B -> T C) (f : A -> T B),
+        bind F g ∘ bind F f = bind F (g ⋆1 f).
+    Proof.
+      introv. unfold transparent tcs. unfold kc1.
+      unfold_ops @Bind_Join.
+      rewrite <- 2(fun_map_map (F := F)).
+      reassociate <- on right.
+      change (map ?F (map ?T g)) with (map (F ∘ T) g).
+      reassociate <- on right.
+      rewrite <- (mod_action_action (U := T) (T := F)).
+      reassociate -> near (map (F ∘ T) g).
+      now rewrite <- (natural (ϕ := @right_action F T _)).
+    Qed.
+
+  End Monad_kleisli_laws.
+
+  #[local] Instance Kleisli_RightModule {F T} `{Categorical.RightModule.RightModule F T}
+    : Kleisli.Monad.RightModule T F :=
+    {| kmod_bind1 := @bind_id T F _ _ _ _ _ _;
+      kmod_bind2 := @bind_bind T F _ _ _ _ _ _;
+    |}.
+
+  (** ** Specification for <<map>>  *)
+  (******************************************************************************)
+  Section Monad_suboperations.
+
+    Context
+      (T : Type -> Type)
+     `{Categorical.Monad.Monad T}.
+
+    (** *** [map] as a special case of [bind]. *)
+    Lemma map_to_bind {A B} : forall `(f : A -> B),
+        map T f = bind T (ret T B ∘ f).
+    Proof.
+      intros. unfold transparent tcs.
+      rewrite <- (fun_map_map (F := T)).
+      reassociate <- on right.
+      now rewrite (mon_join_map_ret (T := T)).
+    Qed.
+
+  End Monad_suboperations.
+
+End ToKleisliRightModule.
