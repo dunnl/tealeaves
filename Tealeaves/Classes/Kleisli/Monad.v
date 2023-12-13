1a2b7376d32e3e01dad27b27f493afe38225d736
Lawrence Dunn <lawrence@comono.id>
2023-12-13 11:15:51 -0500

    Cleanup Kleisli/Monad.v

--- Tealeaves/Classes/Kleisli/Monad.v
+++ Tealeaves/Classes/Kleisli/Monad.v
@@ -4,8 +4,6 @@ From Tealeaves Require Export
   Functors.Identity.
 
 #[local] Generalizable Variables F G A B C D W T U ϕ.
-#[local] Arguments map F%function_scope {Map} (A B)%type_scope f%function_scope _.
-
 Import Functor.Notations.
 
 (** * Monads *)
@@ -19,16 +17,16 @@ Class Return (T : Type -> Type) :=
 Class Bind (U T : Type -> Type) :=
   bind : forall (A B : Type), (A -> U B) -> T A -> T B.
 
-#[global] Arguments ret T%function_scope {Return} A%type_scope.
-#[global] Arguments bind {U} (T)%function_scope {Bind} {A B}%type_scope _%function_scope _.
+#[global] Arguments ret {T}%function_scope {Return} {A}%type_scope.
+#[global] Arguments bind {U} {T}%function_scope {Bind} {A B}%type_scope _%function_scope _.
 
 (** ** Kleisli composition *)
 (******************************************************************************)
-Definition kc1 (T : Type -> Type) `{Return T} `{Bind T T}
+Definition kc1 {T : Type -> Type} `{Return T} `{Bind T T}
   {A B C : Type} (g : B -> T C) (f : A -> T B) : (A -> T C) :=
   @bind T T _ B C g ∘ f.
 
-#[local] Infix "⋆1" := (kc1 _) (at level 60) : tealeaves_scope.
+#[local] Infix "⋆1" := (kc1) (at level 60) : tealeaves_scope.
 
 (** ** Typeclass *)
 (******************************************************************************)
@@ -47,7 +45,7 @@ Class Monad (T : Type -> Type) `{Return T} `{Bind T T} :=
 Class MonadFull (T : Type -> Type) `{Return T} `{Map T} `{Bind T T} :=
   { kmonf_kmon :> Monad T;
     kmonf_map_to_bind : forall `(f : A -> B),
-      @map T _ A B f = @bind T T _ A B (ret T B ∘ f);
+      @map T _ A B f = @bind T T _ A B (ret ∘ f);
   }.
 
 (** ** Homomorphisms *)
@@ -120,7 +118,7 @@ Section derived_instances.
   (** ** Composition between [bind] and [map] *)
   (******************************************************************************)
   Lemma bind_map : forall `(g : B -> T C) `(f : A -> B),
-      @bind T T _ B C g ∘ map T A B f = @bind T T _ A C (g ∘ f).
+      bind g ∘ map f = bind (g ∘ f).
   Proof.
     intros. rewrite (kmonf_map_to_bind).
     rewrite (kmon_bind2). unfold kc1.
@@ -128,7 +126,7 @@ Section derived_instances.
   Qed.
 
   Corollary map_bind : forall `(g : B -> C) `(f : A -> T B),
-      map T B C g ∘ bind T f = bind T (map T B C g ∘ f).
+      map g ∘ bind f = bind (map g ∘ f).
   Proof.
     intros. rewrite (kmonf_map_to_bind).
     now rewrite (kmon_bind2).
@@ -137,7 +135,7 @@ Section derived_instances.
   (** ** Functor laws *)
   (******************************************************************************)
   Lemma map_id : forall (A : Type),
-      map T A A id = id.
+      map (@id A) = id.
   Proof.
     intros.
     rewrite (kmonf_map_to_bind).
@@ -146,7 +144,7 @@ Section derived_instances.
   Qed.
 
   Lemma map_map : forall (A B C : Type) (f : A -> B) (g : B -> C),
-      map T B C g ∘ map T A B f = map T A C (g ∘ f).
+      map g ∘ map f = map (g ∘ f).
   Proof.
     intros.
     rewrite 3(kmonf_map_to_bind).
@@ -164,21 +162,21 @@ Section derived_instances.
   (** ** Special cases for Kleisli composition *)
   (******************************************************************************)
   Lemma kc1_00 : forall `(g : B -> C) `(f : A -> B),
-      (ret T C ∘ g) ⋆1 (ret T B ∘ f) = ret T C ∘ (g ∘ f).
+      (ret ∘ g) ⋆1 (ret ∘ f) = ret ∘ (g ∘ f).
   Proof.
     intros. unfold kc1.
     reassociate <-. now rewrite (kmon_bind0).
   Qed.
 
   Lemma kc1_10 : forall `(g : B -> T C) `(f : A -> B),
-      g ⋆1 (ret T B ∘ f) = g ∘ f.
+      g ⋆1 (ret ∘ f) = g ∘ f.
   Proof.
     intros. unfold kc1.
     reassociate <-. now rewrite (kmon_bind0).
   Qed.
 
   Lemma kc1_01 : forall `(g : B -> C) `(f : A -> T B),
-      (ret T C ∘ g) ⋆1 f = map T B C g ∘ f.
+      (ret ∘ g) ⋆1 f = map g ∘ f.
   Proof.
     intros. unfold kc1.
     rewrite (kmonf_map_to_bind).
@@ -188,7 +186,7 @@ Section derived_instances.
   (** ** Other rules for Kleisli composition *)
   (******************************************************************************)
   Lemma kc1_asc1 : forall `(g : B -> C) `(h : C -> T D) `(f : A -> T B),
-      (h ∘ g) ⋆1 f = h ⋆1 (map T B C g ∘ f).
+      (h ∘ g) ⋆1 f = h ⋆1 (map g ∘ f).
   Proof.
     intros. unfold kc1.
     reassociate <-.
@@ -246,5 +244,5 @@ Qed.
 (** * Notations *)
 (******************************************************************************)
 Module Notations.
-  Infix "⋆1" := (kc1 _) (at level 60) : tealeaves_scope.
+  Infix "⋆1" := (kc1) (at level 60) : tealeaves_scope.
 End Notations.
