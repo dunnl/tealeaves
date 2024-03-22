7d4b1b04dd62496c8d24c978c4debedf6a8cd3e5
Lawrence Dunn <lawrence@comono.id>
2024-03-22 17:26:17 -0400

    Add Backwards applicative instance

--- Tealeaves/Functors/Backwards.v
+++ Tealeaves/Functors/Backwards.v
@@ -10,30 +10,102 @@ Import Applicative.Notations.
 Section Backwards.
 
   Record Backwards (F : Type -> Type) A :=
-    { forwards : F A }.
+    mkBackwards { forwards : F A }.
 
-  Context
-    `{Applicative F}.
+  #[global] Arguments mkBackwards {F}%function_scope {A}%type_scope forwards.
+  #[global] Arguments forwards {F}%function_scope {A}%type_scope b.
 
-  #[export] Instance Map_Backwards : Map (Backwards F) :=
-    fun A B f '(Build_Backwards _ _ x) => {| forwards := map f x |}.
+  Section functor.
 
-  #[export] Instance Pure_Backwards : Pure (Backwards F) :=
-    fun A a => Build_Backwards _ _ (pure a).
+    Context `{Functor F}.
 
-  Definition swap {A B} : B * A -> A * B :=
-    fun '(b, a) => (a, b).
+    #[export] Instance Map_Backwards: Map (Backwards F) :=
+      fun A B f b => mkBackwards (map f (forwards b)).
 
-  Definition mult_Backwards {A B} : Backwards F A -> Backwards F B -> Backwards F (A * B) :=
-    fun ba bb => Build_Backwards F _ (map swap (forwards _ _ bb ⊗ forwards _ _ ba)).
+    #[export] Instance Functor_Backwards: Functor (Backwards F).
+    Proof.
+      constructor.
+      - intros. ext [x].
+        unfold_ops Map_Backwards.
+        rewrite fun_map_id.
+        reflexivity.
+      - intros. ext [x].
+        unfold_ops Map_Backwards.
+        rewrite <- fun_map_map.
+        reflexivity.
+    Qed.
 
-  #[export] Instance Mult_Backwards : Mult (Backwards F) :=
-    fun A B '(x, y) => mult_Backwards x y.
+  End functor.
 
-  (* TODO Finish proving <<Backwards>> is an applicative.
-     We do not need this right now. *)
-  (*
-  #[export, program] Instance Applicative_Backwards : Applicative (Backwards F).
-   *)
+  Section applicative.
+
+    Context
+      `{Applicative F}.
+
+    #[export] Instance Pure_Backwards : Pure (Backwards F) :=
+      fun A a => mkBackwards (pure a).
+
+    Definition swap {A B} : B * A -> A * B :=
+      fun '(b, a) => (a, b).
+
+    Definition mult_Backwards {A B} :
+      Backwards F A -> Backwards F B -> Backwards F (A * B) :=
+      fun ba bb => mkBackwards (map swap (forwards bb ⊗ forwards ba)).
+
+    #[export] Instance Mult_Backwards : Mult (Backwards F) :=
+      fun A B '(x, y) => mult_Backwards x y.
+
+    #[export] Instance Applicative_Backwards : Applicative (Backwards F).
+    Proof.
+      constructor;
+        intros;
+        unfold_ops Pure_Backwards;
+        unfold_ops @Map_Backwards;
+        unfold_ops @Mult_Backwards;
+        unfold mult_Backwards;
+        cbn.
+      - typeclasses eauto.
+      - now rewrite app_pure_natural.
+      - fequal.
+        rewrite app_mult_natural.
+        compose near (forwards y ⊗ forwards x).
+        do 2 rewrite fun_map_map.
+        fequal.
+        now ext [p q].
+      - fequal.
+        rewrite (app_mult_natural_r F).
+        rewrite (app_mult_natural_l F).
+        rewrite <- (app_assoc).
+        compose near (forwards z ⊗ forwards y ⊗ forwards x) on left.
+        compose near (forwards z ⊗ forwards y ⊗ forwards x) on left.
+        compose near (forwards z ⊗ forwards y ⊗ forwards x) on left.
+        compose near (forwards z ⊗ forwards y ⊗ forwards x) on right.
+        repeat rewrite (fun_map_map).
+        fequal.
+        ext [[c b] a].
+        reflexivity.
+      - destruct x as [forward].
+        cbn.
+        fequal.
+        compose near (forward ⊗ pure tt).
+        rewrite (fun_map_map).
+        replace (left_unitor ∘ swap) with (@right_unitor A).
+        now rewrite app_unital_r.
+        now ext [a tt].
+      - destruct x as [forward].
+        cbn.
+        fequal.
+        compose near (pure tt ⊗ forward).
+        rewrite (fun_map_map).
+        replace (right_unitor ∘ swap) with (@left_unitor A).
+        now rewrite app_unital_l.
+        now ext [a tt].
+      - fequal.
+        rewrite app_mult_pure.
+        rewrite app_pure_natural.
+        reflexivity.
+    Qed.
+
+  End applicative.
 
 End Backwards.
