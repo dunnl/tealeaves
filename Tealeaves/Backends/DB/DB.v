b009a2ddb88c990e128b380103cbf51ed58032fb
Lawrence Dunn <lawrence@comono.id>
2024-04-24 18:41:29 -0400

    Sketch some lemmas beyond Autosubst

--- Tealeaves/Backends/DB/DB.v
+++ Tealeaves/Backends/DB/DB.v
@@ -73,77 +73,80 @@ Section section.
       `{Bindd_U_inst : Bindd nat T U}
       `{ToCtxset_U_inst : ToCtxset nat U}.
 
-  Definition bound_in_gap (gap: nat) : nat -> nat -> bool :=
+  Definition bound_within (gap: nat) : nat -> nat -> bool :=
     fun ix depth => ix <? depth + gap.
 
-  Definition bound_in: nat -> nat -> bool :=
-    bound_in_gap 0.
+  Definition bound: nat -> nat -> bool :=
+    bound_within 0.
 
-  Definition closed_gap_loc (gap: nat) (p: nat * nat): bool :=
+  Definition cl_at_loc (gap: nat) (p: nat * nat): bool :=
     match p with
-    | (depth, ix) => bound_in_gap gap ix depth
+    | (depth, ix) => bound_within gap ix depth
     end.
 
-  Definition closed_loc (p: nat * nat): bool :=
-    closed_gap_loc 0 p.
-
-  Definition closed_gap (gap: nat) (t: U nat): Prop :=
+  Definition cl_at (gap: nat) (t: U nat): Prop :=
     forall (depth ix: nat),
       (depth, ix) ∈d t ->
-      closed_gap_loc gap (depth, ix) = true.
+      cl_at_loc gap (depth, ix) = true.
 
   Definition closed (t: U nat): Prop :=
-    closed_gap 0 t.
+    cl_at 0 t.
 
 End section.
 
+Infix "`bound in`" := (bound) (at level 10).
+
 (** ** Boundedness and closure *)
 (******************************************************************************)
-Lemma bound_in_zero: forall ix,
-    bound_in ix 0 = false.
+Lemma bound_within_spec: forall ix d k,
+    bound_within k ix d =
+      ix `bound in` (d + k).
+Proof.
+  intros. cbn. replace (d + k + 0) with (d + k) by lia.
+  reflexivity.
+Qed.
+
+Lemma bound_zero: forall ix,
+    ix `bound in` 0 = false.
 Proof.
   reflexivity.
 Qed.
 
-Lemma bound_in_lt: forall ix n,
-    ix < n ->
-    bound_in ix n = true.
+Lemma bound_lt: forall ix n,
+    ix < n -> ix `bound in` n = true.
 Proof.
   introv Hlt.
   rewrite <- ltb_lt in Hlt.
-  unfold bound_in, bound_in_gap.
+  unfold bound, bound_within.
   replace (n + 0) with n by lia.
   destruct n; auto.
 Qed.
 
-Lemma bound_in_lt_iff: forall ix n,
-    ix < n <->
-      bound_in ix n = true.
+Lemma bound_lt_iff: forall ix n,
+    ix < n <-> ix `bound in` n = true.
 Proof.
   intros. split.
-  - apply bound_in_lt.
+  - apply bound_lt.
   - cbn. destruct n.
     + inversion 1.
     + cbn. rewrite leb_le. lia.
 Qed.
 
-Lemma bound_in_ge: forall ix n,
-    ix >= n ->
-    bound_in ix n = false.
+Lemma bound_ge: forall ix n,
+    ix >= n -> ix `bound in` n = false.
 Proof.
   introv Hlt.
-  unfold bound_in, bound_in_gap.
+  unfold bound, bound_within.
   replace (n + 0) with n by lia.
   destruct n; cbn; auto.
   now rewrite leb_gt.
 Qed.
 
-Lemma bound_in_ge_iff: forall ix n,
-    ix >= n <->
-      bound_in ix n = false.
+Lemma bound_ge_iff: forall ix n,
+    ix >= n <-> ix `bound in` n = false.
 Proof.
   intros. split.
-  - apply bound_in_ge.
+  - apply bound_ge.
   - cbn. replace (n + 0) with n by lia.
     destruct n.
     + lia.
@@ -152,51 +155,51 @@ Proof.
       lia.
 Qed.
 
-Lemma bound_in_ind: forall ix n (P:Prop),
-    (ix >= n -> bound_in ix n = false -> P) ->
-    (ix < n -> bound_in ix n = true -> P) ->
+Lemma bound_ind: forall ix n (P:Prop),
+    (ix >= n -> ix `bound in` n = false -> P) ->
+    (ix < n -> ix `bound in` n = true -> P) ->
     P.
 Proof.
   introv.
-  rewrite <- bound_in_ge_iff.
-  rewrite <- bound_in_lt_iff.
+  rewrite <- bound_ge_iff.
+  rewrite <- bound_lt_iff.
   introv Hge Hlt.
   compare naturals ix and n; auto.
   apply Hge; lia.
 Qed.
 
-Lemma bound_in_mono: forall ix n m,
+Lemma bound_mono: forall ix n m,
     m > n ->
-    bound_in ix n = true ->
-    bound_in ix m = true.
+    (ix `bound in` n = true) ->
+    ix `bound in` m = true.
 Proof.
   introv Hlt Hbound.
-  rewrite <- bound_in_lt_iff in Hbound.
-  rewrite <- bound_in_lt_iff.
+  rewrite <- bound_lt_iff in Hbound.
+  rewrite <- bound_lt_iff.
   lia.
 Qed.
 
 Lemma bound_rev_mono: forall ix n m,
     m < n ->
-    bound_in ix n = false ->
-    bound_in ix m = false.
+    bound ix n = false ->
+    bound ix m = false.
 Proof.
   introv Hlt Hbound.
-  rewrite <- bound_in_ge_iff in Hbound.
-  rewrite <- bound_in_ge_iff.
+  rewrite <- bound_ge_iff in Hbound.
+  rewrite <- bound_ge_iff.
   lia.
 Qed.
 
-Lemma bound_in_1: forall ix,
-    bound_in ix 1 = true <-> ix = 0.
+Lemma bound_1: forall ix,
+    bound ix 1 = true <-> ix = 0.
 Proof.
   destruct ix; intuition.
 Qed.
 
 Ltac bound_induction :=
   match goal with
-  | |- context[bound_in ?ix ?n] =>
-      apply (bound_in_ind ix n);
+  | |- context[bound ?ix ?n] =>
+      apply (bound_ind ix n);
       let Hord := fresh "Hord" in
       let Hbound := fresh "Hbound" in
       introv Hord Hbound;
@@ -206,9 +209,11 @@ Ltac bound_induction :=
 
 (* Autosubst lift *)
 (* plus with different simplification behaviour *)
+
 Definition lift (x y : nat) : nat := plus x y.
-Arguments lift x y/.
 Notation "( + x )" := (lift x) (format "( + x )").
+#[global] Arguments lift x y/.
+
 
 Lemma lift0 : (+0) = id. reflexivity. Qed.
 
@@ -218,10 +223,10 @@ Proof.
   lia.
 Qed.
 
+(*
 Ltac normalize_lift :=
   rewrite ?lift_comp.
 
-(*
 Lemma lift_scons x f n : (+S n) >>> (x .: f) = (+n) >>> f.
 Proof. reflexivity. Qed.
 
@@ -252,10 +257,10 @@ Section ops.
      depth *)
   Definition lift__ren: nat -> (nat -> nat) -> (nat -> nat) :=
     fun depth ρ ix =>
-      if bound_in ix depth
+      if bound ix depth
       then ix
-      else let normalized_ix := ix - depth
-           in ρ normalized_ix + depth.
+      else let free_ix := ix - depth
+           in ρ free_ix + depth.
 
   Definition local__ren (ρ : nat -> nat) (p: nat * nat): nat :=
     match p with
@@ -269,10 +274,10 @@ Section ops.
        depth *)
   Definition lift__sub: nat -> (nat -> T nat) -> (nat -> T nat) :=
     fun depth σ ix =>
-      if bound_in ix depth
+      if ix `bound in` depth
       then ret ix
-      else let normalized_ix := ix - depth
-           in rename (+ depth) (σ normalized_ix).
+      else let free_ix := ix - depth
+           in rename (+ depth) (σ free_ix).
 
   Definition local__sub (σ : nat -> T nat) (p: nat * nat): T nat :=
     match p with
@@ -311,8 +316,10 @@ Section theory.
       `{! Compat_Mapdt_Binddt nat T T}
       `{! Compat_Bindd_Binddt nat T T}
       `{! Compat_Bindt_Binddt nat T T}
-      `{Monad_inst : ! DecoratedTraversableMonad nat T}
-      `{Map_U_inst : Map U}
+      `{Monad_inst : ! DecoratedTraversableMonad nat T}.
+
+  Context
+    `{Map_U_inst : Map U}
       `{Mapd_U_inst : Mapd nat U}
       `{Traverse_U_inst : Traverse U}
       `{Bind_U_inst : Bind T U}
@@ -747,5 +754,55 @@ Section theory.
     rewrite rename_ret.
     fequal; lia.
   Qed.
+
+  (** * Closure and substitution *)
+  (******************************************************************************)
+  Lemma ind_open_iff : forall l n (t:U nat) σ,
+      (n, l) ∈d subst σ t <-> exists n1 n2 l1,
+        (n1, l1) ∈d t /\ (n2, l) ∈d local__sub σ (n1, l1) /\ n = n1 + n2.
+  Proof.
+    intros. unfold subst in *.
+    now rewrite ind_bindd_iff'.
+  Qed.
+
+  Lemma hm: forall (σ: nat -> T nat) (k d1 i1: nat),
+    (forall (n: nat), cl_at k (σ n)) ->
+    cl_at (k + d1) (lift__sub d1 σ i1).
+    Proof.
+    intros.
+    unfold cl_at, cl_at_loc in *.
+    unfold lift__sub.
+    remember (i1 `bound in` d1) as b.
+    intros depth ix Hin.
+    rewrite bound_within_spec.
+    setoid_rewrite bound_within_spec in H.
+    setoid_rewrite <- bound_lt_iff in H.
+    symmetry in Heqb.
+    destruct b.
+    - rewrite <- bound_lt_iff in Heqb.
+       rewrite ind_ret_iff in Hin.
+      destruct Hin as [Hdepth Hix];
+        subst; unfold_ops @Monoid_unit_zero.
+      rewrite <- bound_lt_iff. lia.
+    - rewrite <- bound_lt_iff.
+      rewrite <- bound_ge_iff in Heqb.
+      specialize (H (i1 - d1) depth ix).
+      admit.
+    Admitted.
+
+  Lemma hmm:
+    forall (t: U nat) (σ: nat -> T nat) (k: nat),
+    (forall (n: nat), cl_at k (σ n)) ->
+    cl_at k (subst σ t).
+  Proof.
+    introv Hprem.
+    unfold cl_at in *.
+    unfold cl_at_loc in *.
+    introv Hin.
+    rewrite ind_open_iff in Hin.
+    destruct Hin as [d1 [d2 [i1 [H1 [H2 H3]]]]].
+    cbn.
+  Admitted.
+
 
 End theory.
