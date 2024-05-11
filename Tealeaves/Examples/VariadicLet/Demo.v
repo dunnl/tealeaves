94b0a9bc4d7253a0fdfc14d04c50c7d7875db92e
Lawrence Dunn <lawrence@comono.id>
2024-05-11 17:51:25 -0400

    Update VariadicLet terms to match JAR paper

--- Tealeaves/Examples/VariadicLet/Demo.v
+++ Tealeaves/Examples/VariadicLet/Demo.v
@@ -13,10 +13,12 @@ Definition term1: term LN :=
 Definition term2: term LN :=
   letin [tvar (Bd 0)] (tvar (Bd 0)).
 Definition term3: term LN :=
-  letin [tvar (Fr x); tvar (Bd 1)] (tvar (Bd 1)).
+  letin [tvar (Fr x); tvar (Bd 0)] (tvar (Bd 0)).
 Definition term4: term LN :=
-  letin [tvar (Bd 0); tvar (Bd 1) ; tvar (Bd 2)] (tvar (Bd 2)).
+  letin [tvar (Fr x); tvar (Bd 1)] (tvar (Bd 0)).
 Definition term5: term LN :=
+  letin [tvar (Fr x); tvar (Bd 0) ; tvar (Bd 1)] (tvar (Bd 2)).
+Definition term6: term LN :=
   letin [tvar (Bd 1) ; tvar (Bd 2) ; tvar (Bd 3)] (tvar (Bd 2)).
 
 Module demo1.
@@ -28,18 +30,19 @@ Module demo1.
   Goal LCnb 0 term2 = false. reflexivity. Qed.
   Goal LCnb 1 term2 = true. reflexivity. Qed.
 
-  Goal LCnb 0 term3 = false. reflexivity. Qed.
-  Goal LCnb 1 term3 = false. reflexivity. Qed.
+  Goal LCnb 0 term4 = false. reflexivity. Qed.
+  Goal LCnb 1 term4 = false. reflexivity. Qed.
 
-  Goal LCnb 0 term5 = false. reflexivity. Qed.
-  Goal LCnb 1 term5 = false. reflexivity. Qed.
-  Goal LCnb 2 term5 = false. reflexivity. Qed.
+  Goal LCnb 0 term6 = false. reflexivity. Qed.
+  Goal LCnb 1 term6 = false. reflexivity. Qed.
+  Goal LCnb 2 term6 = false. reflexivity. Qed.
 
   Example ex1: level term1 = 0 := ltac:(trivial).
   Example ex2: level term2 = 1 := ltac:(trivial).
-  Example ex3: level term3 = 2 := ltac:(trivial).
-  Example ex4: level term4 = 3 := ltac:(trivial).
-  Example ex5: level term5 = 4 := ltac:(trivial).
+  Example ex23: level term3 = 1 := ltac:(trivial).
+  Example ex3: level term4 = 2 := ltac:(trivial).
+  Example ex4: level term5 = 2 := ltac:(trivial).
+  Example ex5: level term6 = 4 := ltac:(trivial).
 
 End demo1.
 
@@ -49,9 +52,10 @@ Module demo2.
 
   Example ex1: level term1 = 0 := ltac:(trivial).
   Example ex2: level term2 = 1 := ltac:(trivial).
-  Example ex3: level term3 = 1 := ltac:(trivial).
-  Example ex4: level term4 = 1 := ltac:(trivial).
-  Example ex5: level term5 = 2 := ltac:(trivial).
+  Example ex23: level term3 = 0 := ltac:(trivial).
+  Example ex3: level term4 = 1 := ltac:(trivial).
+  Example ex4: level term5 = 0 := ltac:(trivial).
+  Example ex5: level term6 = 2 := ltac:(trivial).
 
 End demo2.
 
@@ -63,12 +67,13 @@ Module demo3.
 
   Goal LCnb 0 term2 = true. reflexivity. Qed.
 
-  Goal LCnb 0 term3 = true. reflexivity. Qed.
+  Goal LCnb 0 term4 = true. reflexivity. Qed.
 
 
   Example ex1: level term1 = 0 := ltac:(trivial).
   Example ex2: level term2 = 0 := ltac:(trivial).
-  Example ex3: level term3 = 0 := ltac:(trivial).
-  Example ex4: level term4 = 0 := ltac:(trivial).
-  Example ex5: level term5 = 1 := ltac:(trivial).
+  Example ex23: level term3 = 0 := ltac:(trivial).
+  Example ex3: level term4 = 0 := ltac:(trivial).
+  Example ex4: level term5 = 0 := ltac:(trivial).
+  Example ex5: level term6 = 1 := ltac:(trivial).
 End demo3.
