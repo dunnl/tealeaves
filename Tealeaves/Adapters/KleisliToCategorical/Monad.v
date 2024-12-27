ddc5cb2fba2b2c943f0e80771530d78d52f85add
Lawrence Dunn <dunnla@seas.upenn.edu>
2024-12-26 19:01:40 -0500

    Put Kleisli~>Categorical Monad instance into a module

--- Tealeaves/Adapters/KleisliToCategorical/Monad.v
+++ Tealeaves/Adapters/KleisliToCategorical/Monad.v
@@ -9,52 +9,56 @@ From Tealeaves Require Export
   `{Return T} `{Bind T T} : Join T :=
   fun A => bind (@id (T A)).
 
-Section proofs.
+Module Instance.
 
-  Context
-    (T : Type -> Type)
-    `{Classes.Kleisli.Monad.Monad T}.
+  Section proofs.
 
-  Existing Instance Map_Bind.
-  Existing Instance Functor_Monad.
-  Existing Instance MonadFull_Monad.
+    Context
+      (T : Type -> Type)
+        `{Classes.Kleisli.Monad.Monad T}.
 
-  Instance: Classes.Categorical.Monad.Monad T.
-  Proof.
-    constructor.
-    -
-      typeclasses eauto.
-    - constructor.
-      typeclasses eauto.
-      typeclasses eauto.
-      intros. unfold transparent tcs.
-      now rewrite (kmon_bind0 (T := T)).
-    - constructor.
-      typeclasses eauto.
-      typeclasses eauto.
-      intros. unfold transparent tcs.
-      unfold_compose_in_compose.
-      do 2 rewrite (kmon_bind2 (T := T)).
-      fequal. unfold kc1.
-      reassociate <- on right.
-      now rewrite (kmon_bind0 (T := T)).
-    - intros. unfold transparent tcs.
-      unfold_compose_in_compose.
-      now rewrite (kmon_bind0 (T := T)).
-    - intros. unfold transparent tcs.
-      unfold_compose_in_compose.
-      rewrite (kmon_bind2 (T := T)).
-      unfold kc1.
-      reassociate <- near (bind (@id (T A))).
-      rewrite (kmon_bind0 (T := T)). unfold compose.
-      now rewrite <- (kmon_bind1 (T := T) A) at 1.
-    - intros. unfold transparent tcs.
-      unfold_compose_in_compose.
-      rewrite (kmon_bind2 (T := T)).
-      rewrite (kmon_bind2 (T := T)).
-      unfold kc1.
-      reassociate <- near (bind (@id (T A))).
-      now rewrite (kmon_bind0 (T := T)).
-  Qed.
+    Existing Instance Map_Bind.
+    Existing Instance Functor_Monad.
+    Existing Instance MonadFull_Monad.
 
-End proofs.
+    #[local] Instance CategoricalFromKleisli_Monad: Classes.Categorical.Monad.Monad T.
+    Proof.
+      constructor.
+      -
+        typeclasses eauto.
+      - constructor.
+        typeclasses eauto.
+        typeclasses eauto.
+        intros. unfold transparent tcs.
+        now rewrite (kmon_bind0 (T := T)).
+      - constructor.
+        typeclasses eauto.
+        typeclasses eauto.
+        intros. unfold transparent tcs.
+        unfold_compose_in_compose.
+        do 2 rewrite (kmon_bind2 (T := T)).
+        fequal. unfold kc1.
+        reassociate <- on right.
+        now rewrite (kmon_bind0 (T := T)).
+      - intros. unfold transparent tcs.
+        unfold_compose_in_compose.
+        now rewrite (kmon_bind0 (T := T)).
+      - intros. unfold transparent tcs.
+        unfold_compose_in_compose.
+        rewrite (kmon_bind2 (T := T)).
+        unfold kc1.
+        reassociate <- near (bind (@id (T A))).
+        rewrite (kmon_bind0 (T := T)). unfold compose.
+        now rewrite <- (kmon_bind1 (T := T) A) at 1.
+      - intros. unfold transparent tcs.
+        unfold_compose_in_compose.
+        rewrite (kmon_bind2 (T := T)).
+        rewrite (kmon_bind2 (T := T)).
+        unfold kc1.
+        reassociate <- near (bind (@id (T A))).
+        now rewrite (kmon_bind0 (T := T)).
+    Qed.
+
+  End proofs.
+
+End Instance.
