4ee74ab33cf3d6e6f60606ffb2db3df82aabe113
Lawrence Dunn <dunnla@seas.upenn.edu>
2025-04-16 14:57:45 -0400

    Delete files unnecessary for JAR paper

--- Tealeaves/Adapters/PolyToMono/Categorical/DecoratedTraversableMonad.v
+++
@@ -1,5 +0,0 @@
-From Tealeaves Require Export
-  Classes.Categorical.DecoratedTraversableMonad
-  Classes.Categorical.DecoratedTraversableMonadPoly
-  Adapters.PolyToMono.Categorical.Decorated
-  Adapters.PolyToMono.Categorical.DecoratedFunctor.
