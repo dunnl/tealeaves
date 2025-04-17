457581336e9fbec8381459a6fa7879bc77ecda71
Lawrence Dunn <dunnla@seas.upenn.edu>
2025-04-17 13:43:57 -0400

    Build working DecMonPoly.v

--- Tealeaves/Adapters/CategoricalToKleisli/DecoratedMonadPoly.v
+++ Tealeaves/Adapters/CategoricalToKleisli/DecoratedMonadPoly.v
@@ -5,6 +5,7 @@ From Tealeaves Require Import
 #[local] Generalizable Variables T.
 
 Import Monoid.Notations.
+Import Product.Notations.
 
 (** * Kleisli DTMs from Categorical DTMs *)
 (**********************************************************************)
@@ -13,7 +14,6 @@ Import Monoid.Notations.
 (**********************************************************************)
 Module DerivedOperations.
 
-  Print BinddP.
   #[export] Instance BinddP_Categorical
     (T: Type -> Type -> Type)
     `{Map2 T}
@@ -26,6 +26,8 @@ Module DerivedOperations.
 
 End DerivedOperations.
 
+(** ** Derived Instances *)
+(**********************************************************************)
 Module DerivedInstances.
 
   Import DerivedOperations.
@@ -35,108 +37,102 @@ Module DerivedInstances.
   Context
     `{Categorical.DecoratedMonadPoly.DecoratedMonadPoly T}.
 
+  #[local] Arguments ret (T)%function_scope {Return} {A}%type_scope _.
+
+  Lemma binddp_ret {B1 B2 V1 V2}:
+    forall (ρ: list B1 * B1 -> B2)
+      (σ: list B1 * V1 -> T B2 V2),
+      binddp ρ σ ∘ ret (T B1) = σ ∘ ret ((list B1) ×).
+  Proof.
+    intros.
+    unfold binddp, BinddP_Categorical.
+    reassociate -> on left.
+    rewrite dmp_dec_ret.
+    reassociate <- on left.
+    reassociate -> near (ret (T (Z B1))).
+    unfold_compose_in_compose.
+    rewrite dmp_map_ret.
+    reassociate <- on left.
+    setoid_rewrite mon_join_ret.
+    reflexivity.
+  Qed.
+
+  Lemma binddp_id {B V}:
+    binddp extract (ret (T B) ∘ extract) = @id (T B V).
+  Proof.
+    unfold binddp, BinddP_Categorical.
+    rewrite <- fun2_map21_map2.
+    reassociate <-.
+    reassociate -> on left.
+    rewrite dfunp_dec_extract.
+    change (?x ∘ id) with x.
+    apply mon_join_map_ret.
+  Qed.
+
+  Lemma binddp_composition_law:
+    forall {B1 B2 B3 V1 V2 V3: Type}
+      (ρ1: list B1 * B1 -> B2)
+      (ρ2: list B2 * B2 -> B3)
+      (σ1: list B1 * V1 -> T B2 V2)
+      (σ2: list B2 * V2 -> T B3 V3),
+      map2 (ρ2 ∘ map ρ1 ∘ cojoin (W := Z)) (join ∘ (map2 ρ2 σ2 ∘ (shift2 ∘ map_snd decp) ∘ map2 ρ1 σ1 ∘ cojoin_Z2)) =
+        map2 (ρ2 ∘ cobind (W := Z) ρ1) (kc_dmp ρ1 σ1 ρ2 σ2).
+  Proof.
+    intros.
+    unfold kc_dmp.
+    rewrite cobind_Z_spec.
+    fequal.
+    ext [ctx v].
+    unfold binddp, BinddP_Categorical.
+    unfold compose.
+    cbn.
+    unfold id.
+    unfold shift2.
+    unfold compose.
+    unfold strength2.
+    compose near (decp (σ1 (ctx, v))) on left.
+    rewrite fun2_map_map.
+    compose near (decp (σ1 (ctx, v))) on left.
+    rewrite fun2_map_map.
+    rewrite mapd_list_prefix_spec.
+    unfold preincr.
+    reflexivity.
+  Qed.
+
   #[export] Instance: Kleisli.DecoratedMonadPoly.DecoratedMonadPoly T.
   Proof.
     constructor; intros.
-    - unfold binddp, BinddP_Categorical.
-      reassociate -> on left.
-      rewrite xxx_dec_ret.
-      reassociate <- on left.
-      fequal.
-      reassociate ->.
-      unfold_compose_in_compose.
-      rewrite xxx_map_ret.
-      reassociate <- on left.
-      change σ with (id ∘ σ) at 2.
-      fequal.
-      setoid_rewrite mon_join_ret.
-      reflexivity.
-    - unfold binddp, BinddP_Categorical.
-      rewrite <- fun2_map21_map2.
-      reassociate <-.
-      reassociate -> on left.
-      rewrite dfunp_dec_extract.
-      change (?x ∘ id) with x.
-      apply mon_join_map_ret.
+    - apply binddp_ret.
+    - apply binddp_id.
     - unfold binddp, BinddP_Categorical.
       repeat reassociate <- on left.
       change_left (join ∘ map2 ρ2 σ2 ∘ (decp ∘ join) ∘ map2 ρ1 σ1 ∘ decp).
-      rewrite xxx_dec_join.
+      rewrite dmp_dec_join.
       repeat reassociate <- on left.
-      change_left (join ∘ map2 ρ2 σ2 ∘ join ∘ map2 id (shift2 ∘ map_snd decp) ∘ (decp ∘ map2 ρ1 σ1) ∘ decp).
-      rewrite polydecnat.
-      repeat reassociate <- on left.
-      reassociate -> on left.
-      rewrite dfunp_dec_dec.
-      change_left (join ∘ map2 ρ2 σ2 ∘ join ∘
-                     (map2 id (shift2 ∘ map_snd decp) ∘ map2 (map ρ1) (map2 ρ1 σ1))
-                     ∘ (map2 cojoin cojoin_Z2 ∘ decp)).
-      rewrite fun2_map_map.
-      repeat reassociate <- on left.
-      change_left ( join ∘ map2 ρ2 σ2 ∘ join ∘
-                      (map2 (id ∘ map (F := Z) ρ1) (shift2 ∘ map_snd decp ∘ map2 ρ1 σ1) ∘
-                         map2 (cojoin (W := Z)) cojoin_Z2) ∘ decp).
-      rewrite fun2_map_map.
-      change (id ∘ ?f) with f.
-      change_left (join ∘ (map2 ρ2 σ2 ∘ join) ∘ map2 (map (F := Z) ρ1 ∘ (cojoin (W := Z)))
-                     (shift2 ∘ map_snd decp ∘ map2 ρ1 σ1 ∘ cojoin_Z2) ∘ decp).
-      rewrite xxx_map_join.
+      reassociate -> near (join (T := T (Z B2))).
+      rewrite dmp_map_join.
       reassociate <- on left.
       rewrite mon_join_join.
-      fequal.
-      repeat reassociate ->.
-      fequal.
-      repeat reassociate <-.
-      unfold_compose_in_compose.
-      rewrite (fun2_map21_map2 (F := T)).
-      rewrite fun2_map_map.
-      fequal.
-      + fequal.
-        unfold_ops @Map_Z Cojoin_Z @Cobind_Z.
-        unfold compose.
-        ext [l b]. cbn.
-        unfold id. fequal.
-        now rewrite mapd_list_prefix_spec.
-      + unfold kc_dmp.
-        ext [ctx v].
-        unfold compose.
-        cbn.
-        unfold id.
-        unfold binddp, BinddP_Categorical.
-        fequal.
-        admit.
-
-        unfold preincr, incr, compose.
-        fereflexivity.
-        admit
-      change_left (join ∘ join ∘ map2 ρ2 (map2 ρ2 σ2) ∘ map2 (map ρ1 ∘ cojoin (W := Z))
-                     (shift2 ∘ map_snd decp ∘ map2 ρ1 σ1 ∘ cojoin_Z2) ∘ decp).
-      reassociate -> on left.
-
-      change_left (join ∘ map2 ρ2 σ2 ∘ join ∘ (map2 (id ∘ map ρ1) (shift2 ∘ map_snd decp ∘ map2 ρ1 σ1)
-                     ∘ map2 (cojoin (W := Z)) cojoin_Z2) ∘ decp).
-
-      unfold kc_dmp.
-      unfold binddp, BinddP_Categorical.
-      change (
-
-      unfold bindp.
-      Search join map2.
-      unfold cobind, Cobind_Z.
-      unfold mapd_Z.
-      admit.
-    - unfold substitute.
-      unfold Substitute_Categorical.
-      repeat reassociate <- on left.
-      rewrite appmor_natural_pf.
-      reassociate -> near dist2.
-      unfold compose at 9.
-      rewrite <- (dist2_morph (T := T) (ϕ := ϕ)).
+      reassociate -> near (map2 ρ1 σ1).
+      rewrite polydecnat.
       reassociate <- on left.
-      reassociate -> near (map2 ρ σ).
+      reassociate -> on left.
+      rewrite dfunp_dec_dec.
+      reassociate -> near (map2 id (shift2 ∘ map_snd decp)).
       rewrite fun2_map_map.
+      reassociate -> near (map2 (map ρ1) (map2 ρ1 σ1)).
+      rewrite fun2_map_map.
+      reassociate <- on left.
+      reassociate -> near (map2 cojoin cojoin_Z2).
+      rewrite fun2_map_map.
+      reassociate -> near (map2 (ρ2 ∘ id ∘ map ρ1 ∘ cojoin (W := Z))
+                            (map2 ρ2 σ2 ∘ (shift2 ∘ map_snd decp) ∘ map2 ρ1 σ1 ∘ cojoin_Z2)).
+      rewrite fun2_map21_map2.
+
+      reassociate -> on right.
+      setoid_rewrite <- (binddp_composition_law ρ1 ρ2 σ1 σ2).
       reflexivity.
-  Admitted.
+  Qed.
 
   End section.
 
